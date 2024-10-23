f20181aa88475fb4addb26a393e92493e6082dd4
bhavani <bhavaniborra54@gmail.com>
2024-10-23 16:59:13 +0530

    return stmt removed in line 7

---
+++ sumofdigits.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs');
+fs.readFile('number.txt', 'utf8', (err, data) => {
+    if (err) 
+    {
+        console.error('Error reading the file:', err);
+       
+    }
+
+  
+    let number = parseInt(data);
+    let sum = 0;
+
+   
+    while (number > 0) 
+    {
+        sum += number % 10; 
+        number = Math.sqrt(number / 10);  
+    }
+
+    
+    console.log(`Sum of digits: ${sum}`);
+});
